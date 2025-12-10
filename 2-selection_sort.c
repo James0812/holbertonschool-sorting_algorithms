a51127c150535db3a33d6d004e527b5babd22536
James0812 <james.roussel008@gmail.com>
2025-12-10 13:08:26 +0100

    add the code in repo

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -32,38 +32,4 @@ void selection_sort(int *array, size_t size)
 		}
 	}
 }
-#include "sort.h"
-
-/**
- * selection_sort - Sorts an array using Selection Sort
- * @array: The array to sort
- * @size: The size of the array
- */
-void selection_sort(int *array, size_t size)
-{
-	size_t i, j, min;
-	int temp;
-
-	if (!array || size < 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		min = i;
-
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min])
-				min = j;
-		}
-
-		if (min != i)
-		{
-			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-			print_array(array, size);
-		}
-	}
-}
 
