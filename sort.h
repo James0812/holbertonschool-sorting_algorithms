ffff376d46d11d1669b90019fab2d88adb91b3e1
James0812 <james.roussel008@gmail.com>
2025-12-11 12:12:02 +0100

    add the code in repo

--- sort.h
+++ sort.h
@@ -3,23 +3,19 @@
 
 #include <stddef.h>
 
-/* Doubly linked list node structure */
-typedef struct listint_s
-{
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
-} listint_t;
-
-/* Sorting algorithms */
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-/* Printing functions */
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+/* Structure for doubly linked list used in insertion_sort_list */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
 
 #endif /* SORT_H */
 
